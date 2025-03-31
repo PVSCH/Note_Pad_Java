0a07a70f62ccac5c0756f24741a22abea646bc7d
PVSCH <srichaitanyapasupula2004@gmail.com>
2025-03-31 22:14:36 +0530

    created a file

---
+++ Dialogbox.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import javax.swing.*;
+
+
+class Dialogbox{
+
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("my first notepad");
+
+        Arra
+        
+    }
+}
